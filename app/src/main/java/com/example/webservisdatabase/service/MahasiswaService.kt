d73429d9c67d71f3696d441337534b38539ed1fb
emzhran <zachran750@gmail.com>
2024-12-28 18:31:09 +0700

    materi di kelas

---
+++ app/src/main/java/com/example/webservisdatabase/service/MahasiswaService.kt
@@ -0,0 +1,31 @@
+package com.example.webservisdatabase.service
+
+import com.example.webservisdatabase.model.Mahasiswa
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-type: application/json"
+    )
+    @GET("bacamahasiswa.php")
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    @GET("baca1mahasiswa.php/{nim}")
+    suspend fun getMahasiswaById(@Query("nim") nim:String):Mahasiswa
+
+    @POST("insertmahasiswa.php")
+    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
+
+    @PUT("editmahasiswa.php/{nim}")
+    suspend fun updateMahasiswa(@Query("nim")nim: String, @Body mahasiswa: Mahasiswa)
+
+    @DELETE("deletemahasiswa.php/{nim}")
+    suspend fun deleteMahasiswa(@Query("nim")nim: String):retrofit2.Response<Void>
+}
