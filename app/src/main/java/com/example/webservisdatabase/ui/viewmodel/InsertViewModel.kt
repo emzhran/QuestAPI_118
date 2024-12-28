5f34b5c4694415bb11d285732262714d3d7e9b18
emzhran <zachran750@gmail.com>
2024-12-28 20:11:24 +0700

    membuat insertviewmodel serta menambahkan data class dan fungsi konversi dari mahasiswa

---
+++ app/src/main/java/com/example/webservisdatabase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.webservisdatabase.ui.viewmodel
+
+import com.example.webservisdatabase.model.Mahasiswa
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
