c2ccec7c56427e0e986f083d8697da7e9fb4e2d1
emzhran <zachran750@gmail.com>
2024-12-29 22:06:28 +0700

    membuat MahasiswaApp untuk mengelola halaman aplikasi dan memanggilnya pada MainActivity

--- app/src/main/java/com/example/webservisdatabase/MainActivity.kt
+++ app/src/main/java/com/example/webservisdatabase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.webservisdatabase.ui.theme.WebservisDatabaseTheme
+import com.example.webservisdatabase.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WebservisDatabaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MahasiswaApp()
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
