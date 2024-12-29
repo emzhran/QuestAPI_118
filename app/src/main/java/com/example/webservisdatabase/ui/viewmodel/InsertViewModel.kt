ad68ea853e9efeae7e2962abf45ea8a5a2648216
emzhran <zachran750@gmail.com>
2024-12-29 20:01:03 +0700

    menambahkan data class InsertUiState dan fungsi konversi ke InsertViewModel

--- app/src/main/java/com/example/webservisdatabase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/webservisdatabase/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,10 @@ package com.example.webservisdatabase.ui.viewmodel
 
 import com.example.webservisdatabase.model.Mahasiswa
 
+data class InsertUiSate(
+    val insertUiEvent : InsertUiEvent = InsertUiEvent()
+)
+
 data class InsertUiEvent(
     val nim: String = "",
     val nama: String = "",
@@ -9,6 +13,19 @@ data class InsertUiEvent(
     val jenisKelamin: String = "",
     val kelas: String = "",
     val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs():InsertUiSate = InsertUiSate(
+    insertUiEvent = toInsertUiEvent()
 )
 
 fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
