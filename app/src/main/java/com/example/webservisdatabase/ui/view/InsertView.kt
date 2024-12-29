2d9c5e58ef8c52e36c330ab36034daab3d28f175
emzhran <zachran750@gmail.com>
2024-12-29 20:49:30 +0700

    menambahkan tampilan dan logika EntryMhsScreen untuk input data mahasiswa

--- app/src/main/java/com/example/webservisdatabase/ui/view/InsertView.kt
+++ app/src/main/java/com/example/webservisdatabase/ui/view/InsertView.kt
@@ -4,14 +4,99 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.webservisdatabase.ui.cutstomwidget.CustomeTopAppBarr
+import com.example.webservisdatabase.ui.navigasi.DestinasiNavigasi
+import com.example.webservisdatabase.ui.viewmodel.HomeViewModel
 import com.example.webservisdatabase.ui.viewmodel.InsertUiEvent
+import com.example.webservisdatabase.ui.viewmodel.InsertUiSate
+import com.example.webservisdatabase.ui.viewmodel.InsertViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_mhs"
+    override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack:()->Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomeTopAppBarr(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerpadding->
+        EntryBody(
+            insertUiSate = viewModel.uiSate,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerpadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiSate: InsertUiSate,
+    onSiswaValueChange: (InsertUiEvent)->Unit,
+    onSaveClick:()->Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = Modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiSate.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
