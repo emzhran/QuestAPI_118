cf48bb2d571e2bda30419ad941ea320bfa0ae118
emzhran <zachran750@gmail.com>
2024-12-30 14:22:03 +0700

    menambahkan DetailViewModel dan UpdateViewmModel ke PenyediaViewModel

--- app/src/main/java/com/example/webservisdatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/webservisdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.webservisdatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +11,11 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
         initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer {
+            val savedStateHandle = createSavedStateHandle()
+            DetailViewModel(savedStateHandle, aplikasiMahasiswa().container.mahasiswaRepository)
+        }
+        initializer { UpdateViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
     }
 }
 
